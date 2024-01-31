c68e5b4c28811a20b037ebdbb315aac93f9cca92
Joshua <76941328+joshua-legend@users.noreply.github.com>
2024-01-31 10:48:06 +0900

    Add files via upload

---
+++ day01/var.py
@@ -0,0 +1,19 @@
+# var
+# variable - 변수
+
+# 10 + 50 [연산 + 기억]
+# number = 10 + 50 [연산]
+# myname = input("너의 이름은?")
+# age = 10 + 20
+# print(myname)
+# print(age)
+
+name = input("당신의 이름?")
+age = input("당신의 나이?")
+favorite_food = input("당신의 최애 음식")
+favoriteYoutube = input("당신의 최애 유튜버는?")
+print(name)
+print(age)
+print(favorite_food)
+print(favoriteYoutube)
+
